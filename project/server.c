c686382a6415dd7556e35cdd68d8e5908609c9fc
gpham2 <giangphamgia03@gmail.com>
2023-04-24 21:21:22 -0700

    standard: made server working

--- project/server.c
+++ project/server.c
@@ -1,4 +1,71 @@
-int main(int argc, char *argv[])
-{
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+    int sockfd, newsockfd, portno, clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    // create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    // initialize server address
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    portno = 8080;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    // bind socket to address
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR on binding");
+        exit(1);
+    }
+
+    // listen for incoming connections
+    listen(sockfd, 5);
+
+    while(1) {
+        // accept connection
+        clilen = sizeof(cli_addr);
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            exit(1);
+        }
+
+        // read from socket
+        memset(buffer, 0, 256);
+        n = read(newsockfd, buffer, 255);
+        if (n < 0) {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+        printf("Here is the message: %s\n",buffer);
+
+        // write to socket
+        n = write(newsockfd, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello, World!</h1></body></html>", 111);
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
+
+        // close socket
+        close(newsockfd);
+    }
+
+    // close socket
+    close(sockfd);
+
+    return 0;
 }
